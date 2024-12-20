b315f8bf74cfbee6ac54e88f463b1ad59511c87f
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2024-12-21 00:29:38 +0800

    finish activity 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+  let error = '';
+  if (a < 0 || b < 0) error = 'ERROR';
+  if (Math.floor(a) !== a || Math.floor(b) !== b) error = 'ERROR';
+  if (error) return error 
+  let sum = 0;
+  if (a > b) {
+    const temp = a;
+    a = b;
+    b = temp;
+  }
+  for (let x = a; x <= b; x++) {
+    sum += x;
+  }
+  return sum;
 };
 
 // Do not edit below this line
