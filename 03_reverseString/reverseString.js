f37b06ec9b87aed701d22e84887e8c2ecd5c53f9
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2024-12-20 23:40:02 +0800

    finish activity 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let newWord = '';
+    for (let x = word.length - 1; x >= 0; x--) {
+        newWord += word[x];
+    }
+    return newWord;
 };
 
 // Do not edit below this line
