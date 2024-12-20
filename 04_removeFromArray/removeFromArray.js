75ecadf5454a1b7f31e5dda211eeecaa778709b2
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2024-12-21 00:08:43 +0800

    finish activity 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...num) {
+  const newArr = [];
+  outer:
+  for (let x = 0; x < arr.length; x++) {
+    for (let y = 0; y < num.length; y++) {
+      if (arr[x] === num[y]) 
+        continue outer;
+    }
+    newArr.push(arr[x]);
+  }
+  return newArr;
 };
 
 // Do not edit below this line
