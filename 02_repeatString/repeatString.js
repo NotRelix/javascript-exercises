29a02a64cead4ec2766517f1436d8d1213a92f20
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2024-12-20 14:43:48 +0800

    finish activity 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(word, count) {
+	if (count < 0) {
+		return 'ERROR';
+	}
 
+	let newWord = '';
+	for (let x = 0; x < count; x++) {
+		newWord += word;
+	}
+	
+	return newWord;
 };
 
 // Do not edit below this line
