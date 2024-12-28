c77a83a08a91679b8d51d99104502704eda3690f
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2024-12-28 16:20:17 +0800

    finish activity 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  if (num < 0) return "OOPS";
+  if (num == 0) return 0;
+  if (num == 1) return 1;
+  let curr = 1;
+  let prev = 1;
+  for (let x = 2; x < num; x++) {
+    const temp = curr;
+    curr += prev;
+    prev = temp;
+  }
+  return curr;
 };
 
 // Do not edit below this line
