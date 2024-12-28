8c6b0e6f5bd78e91242ce9c0bbd15f8eaf940d55
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2024-12-28 15:54:43 +0800

    finish activity 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,20 +1,12 @@
 const palindromes = function (word) {
-  let left = 0;
-  let right = word.length - 1;
-  while (left < right) {
-    while (!word[left].match(/[0-9a-zA-Z]/)) {
-      left++;
-    }
-    while (!word[right].match(/[0-9a-zA-Z]/)) {
-      right--;
-    }
-    if (word[left].toLowerCase() !== word[right].toLowerCase()) {
-      return false;
-    } 
-    left++;
-    right--;
-  }
-  return true;
+  const alphanumeric = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  const filtered = word
+    .toLowerCase()
+    .split('')
+    .filter(letter => alphanumeric.includes(letter))
+    .join('')
+                    
+  return filtered === filtered.split('').reverse().join('');
 };
 
 // Do not edit below this line
