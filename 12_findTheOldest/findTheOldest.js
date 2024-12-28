3453fc3e8d01ebb36b7b1936a6631711f8234c57
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2024-12-28 17:06:49 +0800

    finish activity 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+  let oldestPerson = array[0]
+  array.map(person => {
+    const oldestPersonDeath = ('yearOfDeath' in oldestPerson) 
+    ? oldestPerson.yearOfDeath
+    : new Date().getFullYear()
+    
+    const currentPersonDeath = ('yearOfDeath' in person) 
+    ? person.yearOfDeath
+    : new Date().getFullYear()
+    
+    const oldestPersonAge = oldestPersonDeath - oldestPerson.yearOfBirth;
+    const currentPersonAge = currentPersonDeath - person.yearOfBirth;
+    if (currentPersonAge > oldestPersonAge) {
+      oldestPerson = person;
+    }
+  })
+  return oldestPerson
 };
 
 // Do not edit below this line
